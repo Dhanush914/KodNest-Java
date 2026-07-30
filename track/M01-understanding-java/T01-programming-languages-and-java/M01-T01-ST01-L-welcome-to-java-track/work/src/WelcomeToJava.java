1ac8b31eff7577a932faff1b733ec375511094e6
Revanth <revantht003@gmail.com>
2026-07-30 13:05:08 +0530

    one more line added

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -6,5 +6,6 @@ public class WelcomeToJava {
         System.out.println("My name is Revanth");
         System.out.println("My goal is to become a Java Developer");
         System.out.println("I Selected java because i want to build large application");
+        System.out.println("Know i now how to use git and github");
     }
 }
