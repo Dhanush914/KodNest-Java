27e9b962e4f7d8aa2d658cac0135e3a7f5b88bbd
Revanth <revantht003@gmail.com>
2026-08-04 13:00:05 +0530

    condition

---
+++ track/condition.java
@@ -0,0 +1,15 @@
+
+public class condition {
+
+    public static void main(String[] args) {
+        int num = 0;
+
+        if (num > 0) {
+            System.out.println("Positive Number");
+        } else if (num < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
