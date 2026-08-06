d17f197b0e15856301c0375189c3fb8ec15328c2
Revanth <revantht003@gmail.com>
2026-08-06 11:57:17 +0530

    main

---
+++ track/M02-introduction to Java/dowhile.java
@@ -0,0 +1,17 @@
+
+public class dowhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            int j = 1;
+            do {
+                System.out.println(j);
+                j++;
+            } while (j <= 10);
+
+            System.out.println();
+            i++;
+        } while (i <= 10);
+    }
+}
